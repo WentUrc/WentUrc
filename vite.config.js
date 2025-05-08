3037f3fc1bf727ae35ad904838ea873754030d72
IGCrystal <IGCrystal@wenturc.com>
2025-05-08 19:06:44 +0800

    ✨ feat: 支持在局域网开放开发服务器

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '',
   server: {
-    port: 3000
+    host: '0.0.0.0', // 监听所有网络接口
+    port: 3000, // 可选：指定端口
+    open: true, // 可选：启动时自动打开浏览器
   }
 })
